baeb59eada0dacc8e775421abcadf3cb15c69977
Usmanov <roma.maksato@gmail.com>
2022-03-24 08:33:25 +0300

    update the js

--- js/script.js
+++ js/script.js
@@ -19,13 +19,70 @@ if (addInput.value.length === 0){
     li.appendChild(button)
     todoList.appendChild(li)
     addInput.value = ''
-    const deleteBtn = document.querySelectorAll('.delete-btn')
     const listGroupItems = document.querySelectorAll('.list-group-item')
-    deleteBtn.forEach((item, idx) =>{
+    listGroupItems.forEach((item,idx) =>{
         item.addEventListener('click', () =>{
             listGroupItems[idx].remove()
         })
     })
-    console.log(deleteBtn)
 })
+
+
+
+// const todoList = document.querySelector('.')
+// const addBtn = document.querySelector('.add-btn')
+// const addInput = document.querySelector('.add-input')
+//
+// addBtn.addEventListener('click', () =>{
+//     if (addInput.value.length === 0){
+//         alert('Write something')
+//         return
+//     }
+//
+//     const li = document.createElement('li')
+//     li.classList.add('list-group-item', 'd-flex', 'justify-content-between')
+//     const span = document.createElement('span')
+//     span.textContent = addInput.value
+//     const button = document.createElement('button')
+//     button.classList.add('btn',  'btn-danger', 'btn-sm', 'delete-btn')
+//     button.textContent = 'Delete'
+//     li.append(span)
+//     li.append(button)
+//     todoList.append(li)
+//     addInput.value = ''
+//     const listGroupItems = document.querySelectorAll('.list-group-item')
+//     listGroupItems.forEach((item,idx) =>{
+//         item.addEventListener('click', () =>{
+//             listGroupItems[idx].remove()
+//         })
+//     })
+//
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
