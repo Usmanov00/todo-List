076523998eac8ad6218242fa744a6236603c2fa7
Usmanov <roma.maksato@gmail.com>
2022-03-26 23:44:52 +0300

    change the DELETEITEM function

--- js/script.js
+++ js/script.js
@@ -1,169 +1,46 @@
-// const addBtn = document.querySelector('.add-btn')
-// const addInput = document.querySelector('.add-input')
-// const todoList = document.querySelector('.')//todolist
-//
-//
-// addBtn.addEventListener('click', () =>{
-// if (addInput.value.length === 0){
-//     alert('Введите название задачи!')
-//     return
-// }
-//     const li = document.createElement('li')
-//     li.classList.add('list-group-item', 'd-flex', 'justify-content-between')
-//     const span = document.createElement('span')
-//     span.textContent = addInput.value
-//     const button = document.createElement('button')
-//     button.classList.add('btn',  'btn-danger', 'btn-sm', 'delete-btn')
-//     button.textContent = 'Delete'
-//     li.appendChild(span)
-//     li.appendChild(button)
-//     todoList.appendChild(li)
-//     addInput.value = ''
-//     const listGroupItems = document.querySelectorAll('.list-group-item')
-//     listGroupItems.forEach((item,idx) =>{
-//         item.addEventListener('click', () =>{
-//             listGroupItems[idx].remove()
-//         })
-//     })
-// })
-
-
-// const todoList = document.querySelector('.')
-// const addBtn = document.querySelector('.add-btn')
-// const addInput = document.querySelector('.add-input')
-//
-// addBtn.addEventListener('click', () =>{
-//     if (addInput.value.length === 0){
-//         alert('Write something')
-//         return
-//     }
-//
-//     const li = document.createElement('li')
-//     li.classList.add('list-group-item', 'd-flex', 'justify-content-between')
-//     const span = document.createElement('span')
-//     span.textContent = addInput.value
-//     const button = document.createElement('button')
-//     button.classList.add('btn',  'btn-danger', 'btn-sm', 'delete-btn')
-//     button.textContent = 'Delete'
-//     li.append(span)
-//     li.append(button)
-//     todoList.append(li)
-//     addInput.value = ''
-//     const listGroupItems = document.querySelectorAll('.list-group-item')
-//     listGroupItems.forEach((item,idx) =>{
-//         item.addEventListener('click', () =>{
-//             listGroupItems[idx].remove()
-//         })
-//     })
-//
-// })
-
-
 const addBtn = document.querySelector('.add-btn')
 const addInput = document.querySelector('.add-input')
-const todoList = document.querySelector('.todo-list')
+const allMyBox = document.querySelector('.todo-list')
 
-let allTodos =JSON.parse(localStorage.getItem('todos')) || []
 
-const drawItem = (itemText) =>{
+const getItem = () => {
+    if (addInput.value.length === 0) {
+        alert('write something in a line')
+        return
+    }
     const li = document.createElement('li')
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between')
     const span = document.createElement('span')
-    span.textContent = itemText
+    span.textContent = addInput.value
+    console.log(span)
     const button = document.createElement('button')
     button.classList.add('btn', 'btn-danger', 'btn-sm', 'delete-btn')
     button.textContent = 'Delete'
     li.append(span)
     li.append(button)
-    todoList.append(li)
+    allMyBox.append(li)
+    addInput.value = ''
+
 }
 
-allTodos.forEach((todo) =>{
-    drawItem(todo)
-
+addBtn.addEventListener('click', () => {
+    getItem()
 })
 
-addBtn.addEventListener('click', () =>{
-    if (addInput.value.length === 0){
-        alert('wright something')
-        return
-    }
-        addBtn.addEventListener('click', () =>{
-            drawItem(addInput.value)
-            allTodos = [... allTodos, addInput.value]
-            localStorage.setItem('todos', JSON.stringify(allTodos))
-            addInput.value = ''
-            //deleteItem() // не уыерен чта функция должна быть здесь
-        })
-    })
-// addBtn.addEventListener('click', () =>{
-//     drawItem(addInput.value)
-//     allTodos = [... allTodos, addInput.value]
-//     localStorage.setItem('todos', JSON.stringify(allTodos))
-//     addInput.value = ''
-//
-// })
 
-
-
-// addBtn.addEventListener('click', () =>{
-//     if (addInput.value.length === 0){
-//         alert('wright something')
-//         return
-//     }
-//     const li = document.createElement('li')
-//     li.classList.add('list-group-item', 'd-flex', 'justify-content-between')
-//     const span = document.createElement('span')
-//     span.textContent = addInput.value
-//     const button = document.createElement('button')
-//     button.classList.add('btn', 'btn-danger', 'btn-sm', 'delete-btn')
-//     button.textContent = 'Delete'
-//     li.append(span)
-//     li.append(button)
-//     todoList.append(li)
-//     addInput.value = ''
-//     deleteItem()
-// })
 
 const deleteItem = () =>{
-    const deleteBtn = document.querySelectorAll('.delete-btn')
-    deleteBtn.forEach((item, idx) =>{
-        item.addEventListener('click', () =>{
-            deleteBtn[idx].parentElement.remove()
+    const listItemG = document.querySelectorAll('.list-group-item')
+    const deleteButton = document.querySelectorAll('.delete-btn')
+    deleteButton.forEach((item, idx) =>{
+        item.addEventListener('lick', () =>{
+            listItemG[idx].remove()
         })
     })
+    console.log()
 }
+
 deleteItem()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
